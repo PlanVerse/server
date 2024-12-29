b5b0cfad916d1d82e16d2bfd0489b1b3c7a16064
SeoEunGi0802 <lolove08@naver.com>
2024-12-29 19:58:36 +0900

    redis 오류 해소

--- src/main/kotlin/com/planverse/server/common/config/RedisConfig.kt
+++ src/main/kotlin/com/planverse/server/common/config/RedisConfig.kt
@@ -1,21 +1,15 @@
 package com.planverse.server.common.config
 
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.planverse.server.common.util.RedisUtil
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.cache.CacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.redis.cache.RedisCacheConfiguration
-import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.data.redis.connection.RedisPassword
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
-import org.springframework.data.redis.serializer.RedisSerializationContext
 import org.springframework.data.redis.serializer.StringRedisSerializer
-import java.time.Duration
 
 @Configuration
 class RedisConfig(
@@ -30,39 +24,27 @@ class RedisConfig(
 
     @Value("\${spring.data.redis.database}")
     val database: Int,
-
-    val objectMapper: ObjectMapper
 ) {
     @Bean("lettuceConnectionFactory")
     fun lettuceConnectionFactory(): LettuceConnectionFactory {
-        val redisStandaloneConfiguration = RedisStandaloneConfiguration()
-        redisStandaloneConfiguration.hostName = host
-        redisStandaloneConfiguration.port = port
+        val redisStandaloneConfiguration = RedisStandaloneConfiguration(host, port)
         redisStandaloneConfiguration.password = RedisPassword.of(password)
         redisStandaloneConfiguration.database = database
         return LettuceConnectionFactory(redisStandaloneConfiguration)
     }
 
-    @Bean
+    @Bean("redisTemplate")
     fun redisTemplate(@Qualifier("lettuceConnectionFactory") lettuceConnectionFactory: LettuceConnectionFactory): RedisTemplate<String, Any> {
-        return RedisTemplate<String, Any>().apply {
-            this.connectionFactory = lettuceConnectionFactory()
-            this.setEnableTransactionSupport(true)
-            this.keySerializer = StringRedisSerializer()
-            this.valueSerializer = GenericJackson2JsonRedisSerializer(objectMapper)
-        }
-    }
+        val redisTemplate = RedisTemplate<String, Any>()
+        redisTemplate.connectionFactory = lettuceConnectionFactory
+        redisTemplate.setEnableTransactionSupport(true)
+        redisTemplate.keySerializer = StringRedisSerializer()
+        redisTemplate.valueSerializer = StringRedisSerializer()
+        redisTemplate.hashKeySerializer = StringRedisSerializer()
+        redisTemplate.hashValueSerializer = StringRedisSerializer()
 
-    @Bean
-    fun cacheManager(): CacheManager {
-        val configuration = RedisCacheConfiguration.defaultCacheConfig()
-            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer()))
-            .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(GenericJackson2JsonRedisSerializer())) // Serialize 설정
-            .entryTtl(Duration.ofMinutes(2)) // 캐시 기본 TTL 2분
-            .disableCachingNullValues() // 캐싱 NULL 제외
-        return RedisCacheManager.RedisCacheManagerBuilder
-            .fromConnectionFactory(lettuceConnectionFactory())
-            .cacheDefaults(configuration)
-            .build()
+        RedisUtil.setRedisTemplate(redisTemplate)
+
+        return redisTemplate
     }
-}
+}   
--- src/main/kotlin/com/planverse/server/common/util/RedisUtil.kt
+++ src/main/kotlin/com/planverse/server/common/util/RedisUtil.kt
@@ -5,55 +5,52 @@ import org.springframework.stereotype.Component
 import java.util.concurrent.TimeUnit
 
 @Component
-class RedisUtil(
-    redisTemplate: RedisTemplate<String, Any>
-) {
-    init {
-        Companion.redisTemplate = redisTemplate
+object RedisUtil {
+
+    private lateinit var redisTemplate: RedisTemplate<String, Any>
+
+    fun setRedisTemplate(template: RedisTemplate<String, Any>) {
+        redisTemplate = template
     }
 
-    companion object {
-        private lateinit var redisTemplate: RedisTemplate<String, Any>
+    // 데이터 저장
+    fun set(key: String, value: Any) {
+        redisTemplate.opsForValue().set(key, value)
+    }
 
-        // 데이터 저장
-        fun set(key: String, value: Any) {
-            redisTemplate.opsForValue().set(key, value)
-        }
+    // 만료시간이 있는 데이터 저장
+    fun setWithExpiryMs(key: String, value: Any, timeout: Long) {
+        redisTemplate.opsForValue().set(key, value, timeout, TimeUnit.MILLISECONDS)
+    }
 
-        // 만료시간이 있는 데이터 저장
-        fun setWithExpiryMs(key: String, value: Any, timeout: Long) {
-            redisTemplate.opsForValue().set(key, value, timeout, TimeUnit.MILLISECONDS)
-        }
+    fun setWithExpirySec(key: String, value: Any, timeout: Long) {
+        redisTemplate.opsForValue().set(key, value, timeout, TimeUnit.SECONDS)
+    }
 
-        fun setWithExpirySec(key: String, value: Any, timeout: Long) {
-            redisTemplate.opsForValue().set(key, value, timeout, TimeUnit.SECONDS)
-        }
+    fun setWithExpiryMin(key: String, value: Any, timeout: Long) {
+        redisTemplate.opsForValue().set(key, value, timeout, TimeUnit.MINUTES)
+    }
 
-        fun setWithExpiryMin(key: String, value: Any, timeout: Long) {
-            redisTemplate.opsForValue().set(key, value, timeout, TimeUnit.MINUTES)
-        }
+    fun setWithExpiryHour(key: String, value: Any, timeout: Long) {
+        redisTemplate.opsForValue().set(key, value, timeout, TimeUnit.HOURS)
+    }
 
-        fun setWithExpiryHour(key: String, value: Any, timeout: Long) {
-            redisTemplate.opsForValue().set(key, value, timeout, TimeUnit.HOURS)
-        }
+    fun setWithExpiryDay(key: String, value: Any, timeout: Long) {
+        redisTemplate.opsForValue().set(key, value, timeout, TimeUnit.DAYS)
+    }
 
-        fun setWithExpiryDay(key: String, value: Any, timeout: Long) {
-            redisTemplate.opsForValue().set(key, value, timeout, TimeUnit.DAYS)
-        }
+    // 데이터 조회
+    fun get(key: String): Any? {
+        return redisTemplate.opsForValue().get(key)
+    }
 
-        // 데이터 조회
-        fun get(key: String): Any? {
-            return redisTemplate.opsForValue().get(key)
-        }
+    // 키 존재 확인
+    fun has(key: String): Boolean {
+        return redisTemplate.hasKey(key)
+    }
 
-        // 키 존재 확인
-        fun has(key: String): Boolean {
-            return redisTemplate.hasKey(key)
-        }
-
-        // 데이터 삭제
-        fun delete(key: String): Boolean {
-            return redisTemplate.delete(key)
-        }
+    // 데이터 삭제
+    fun delete(key: String): Boolean {
+        return redisTemplate.delete(key)
     }
 }
