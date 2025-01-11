cc86827b1ce67f4fcbe095d5a459f501aba8688f
SeoEunGi0802 <lolove08@naver.com>
2025-01-11 22:24:43 +0900

    cors 수정 진행

--- src/main/kotlin/com/planverse/server/common/config/WebMvcConfig.kt
+++ src/main/kotlin/com/planverse/server/common/config/WebMvcConfig.kt
@@ -3,6 +3,7 @@ package com.planverse.server.common.config
 import com.planverse.server.common.config.security.CustomAuthenticationPrincipalArgumentResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
@@ -12,5 +13,14 @@ class WebMvcConfig(
 
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(customAuthenticationPrincipalArgumentResolver)
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedOrigins("*")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600)
     }
 }
