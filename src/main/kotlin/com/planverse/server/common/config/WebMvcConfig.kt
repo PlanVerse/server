850149c44a083485464ad9b7ca40d91e5101f044
SeoEunGi0802 <lolove08@naver.com>
2025-01-12 01:59:49 +0900

    불필요 설정 코드 제거

--- src/main/kotlin/com/planverse/server/common/config/WebMvcConfig.kt
+++ src/main/kotlin/com/planverse/server/common/config/WebMvcConfig.kt
@@ -3,7 +3,6 @@ package com.planverse.server.common.config
 import com.planverse.server.common.config.security.CustomAuthenticationPrincipalArgumentResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
-import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
@@ -14,13 +13,4 @@ class WebMvcConfig(
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(customAuthenticationPrincipalArgumentResolver)
     }
-
-//    override fun addCorsMappings(registry: CorsRegistry) {
-//        registry.addMapping("/**")
-//            .allowedMethods("GET", "POST")
-//            .allowedOriginPatterns("*")
-//            .allowedHeaders("*")
-//            .allowCredentials(true)
-//            .maxAge(3600)
-//    }
 }
