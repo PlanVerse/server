ab77bc3a7fc82fa80d46ee4a6c3cce37e1e1440d
SeoEunGi0802 <lolove08@naver.com>
2025-01-12 21:42:04 +0900

    response 값 null 제거 전역화 롤백

--- src/main/kotlin/com/planverse/server/team/dto/TeamMemberInfoDTO.kt
+++ src/main/kotlin/com/planverse/server/team/dto/TeamMemberInfoDTO.kt
@@ -1,7 +1,9 @@
 package com.planverse.server.team.dto
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.planverse.server.team.entity.TeamMemberInfoEntity
 
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 data class TeamMemberInfoDTO(
     var id: Long? = null,
     var userInfoId: Long,
