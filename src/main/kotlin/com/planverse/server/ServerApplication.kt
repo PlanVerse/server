f7e36bc4f4283b2576da2dc2abb2ef58c444f299
SeoEunGi0802 <lolove08@naver.com>
2025-01-23 04:20:20 +0900

    기본 timezone 변경

--- src/main/kotlin/com/planverse/server/ServerApplication.kt
+++ src/main/kotlin/com/planverse/server/ServerApplication.kt
@@ -1,8 +1,10 @@
 package com.planverse.server
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import java.util.*
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
@@ -10,4 +12,9 @@ class ServerApplication
 
 fun main(args: Array<String>) {
     runApplication<ServerApplication>(*args)
+}
+
+@PostConstruct
+fun setTimeZone() {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 }
