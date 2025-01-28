e621f58bb5efa7701216e8be11ce7035d2b846dd
SeoEunGi0802 <lolove08@naver.com>
2025-01-29 02:27:27 +0900

    async 설정 수정 불필요 유틸 제거

--- src/main/kotlin/com/planverse/server/common/config/AsyncConfig.kt
+++ src/main/kotlin/com/planverse/server/common/config/AsyncConfig.kt
@@ -1,6 +1,8 @@
 package com.planverse.server.common.config
 
+import org.slf4j.MDC
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.task.TaskDecorator
 import org.springframework.scheduling.annotation.AsyncConfigurer
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
@@ -14,9 +16,22 @@ class AsyncConfig : AsyncConfigurer {
         threadPoolTaskExecutor.corePoolSize = 1
         threadPoolTaskExecutor.maxPoolSize = 5
         threadPoolTaskExecutor.queueCapacity = 10
+        threadPoolTaskExecutor.setTaskDecorator(LoggingTaskDecorator())
         threadPoolTaskExecutor.setThreadNamePrefix("Async-ThreadPool-Executor-")
         threadPoolTaskExecutor.initialize()
 
         return threadPoolTaskExecutor
+    }
+}
+
+class LoggingTaskDecorator : TaskDecorator {
+    override fun decorate(task: Runnable): Runnable {
+        val callerThreadContext = MDC.getCopyOfContextMap()
+        return Runnable {
+            callerThreadContext?.let {
+                MDC.setContextMap(it)
+            }
+            task.run()
+        }
     }
 }
