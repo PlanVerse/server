06cf0f64c54214594899bbee2742984bf6a58696
seoeungi <sjlim02255@gmail.com>
2025-04-11 15:59:22 +0900

    AuthenticationPrincipal 대상 객체 필드변수 추가

--- src/main/kotlin/com/planverse/server/user/dto/UserInfo.kt
+++ src/main/kotlin/com/planverse/server/user/dto/UserInfo.kt
@@ -1,17 +1,28 @@
 package com.planverse.server.user.dto
 
+import com.planverse.server.user.entity.UserInfoEntity
 import io.jsonwebtoken.Claims
 
 data class UserInfo(
     var id: Long? = null,
+    val key: String,
     val email: String,
     val accessToken: String? = null,
 ) {
     companion object {
-        fun toDto(claims: Claims, accessToken: String): UserInfo {
+        fun toDto(claims: Claims, accessToken: String, email: String): UserInfo {
             return UserInfo(
-                email = claims["sub"].toString(),
-                accessToken = accessToken
+                key = claims["sub"].toString(),
+                email = email,
+                accessToken = accessToken,
+            )
+        }
+
+        fun toDto(userInfoEntity: UserInfoEntity, accessToken: String): UserInfo {
+            return UserInfo(
+                key = userInfoEntity.key!!,
+                email = userInfoEntity.email,
+                accessToken = accessToken,
             )
         }
     }
