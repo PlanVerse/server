31ad5e8fa8bd0d2001bb66a4112120acb053ae72
seoeungi <sjlim02255@gmail.com>
2025-04-12 03:18:57 +0900

    - entity 추가

--- src/main/kotlin/com/planverse/server/comment/entity/CommentInfoEntity.kt
+++ src/main/kotlin/com/planverse/server/comment/entity/CommentInfoEntity.kt
@@ -2,8 +2,13 @@ package com.planverse.server.comment.entity
 
 import com.planverse.server.common.entity.BaseEntity
 import jakarta.persistence.*
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
+import org.hibernate.annotations.ColumnDefault
 import org.hibernate.annotations.DynamicInsert
 import org.hibernate.annotations.DynamicUpdate
+import org.hibernate.annotations.JdbcTypeCode
+import org.hibernate.type.SqlTypes
 
 @Entity
 @DynamicInsert
@@ -14,4 +19,24 @@ class CommentInfoEntity(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     var id: Long? = null,
+
+    @Size(max = 255)
+    @NotNull
+    @ColumnDefault("(gen_random_uuid())")
+    @Column(name = "key", nullable = false)
+    var key: String? = null,
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "target", nullable = false)
+    var target: String,
+
+    @NotNull
+    @Column(name = "target_id", nullable = false)
+    var targetId: Long,
+
+    @NotNull
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(name = "content", nullable = false)
+    var content: List<Map<String, Any>>? = emptyList()
 ) : BaseEntity()
--- src/main/kotlin/com/planverse/server/reply/entity/ReplyInfoEntity.kt
+++ src/main/kotlin/com/planverse/server/reply/entity/ReplyInfoEntity.kt
@@ -2,8 +2,13 @@ package com.planverse.server.reply.entity
 
 import com.planverse.server.common.entity.BaseEntity
 import jakarta.persistence.*
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
+import org.hibernate.annotations.ColumnDefault
 import org.hibernate.annotations.DynamicInsert
 import org.hibernate.annotations.DynamicUpdate
+import org.hibernate.annotations.JdbcTypeCode
+import org.hibernate.type.SqlTypes
 
 @Entity
 @DynamicInsert
@@ -14,4 +19,19 @@ class ReplyInfoEntity(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     var id: Long? = null,
+
+    @Size(max = 255)
+    @NotNull
+    @ColumnDefault("(gen_random_uuid())")
+    @Column(name = "key", nullable = false)
+    var key: String? = null,
+
+    @NotNull
+    @Column(name = "comment_info_id", nullable = false)
+    var commentInfoId: Long,
+
+    @NotNull
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(name = "content", nullable = false)
+    var content: List<Map<String, Any>>? = emptyList()
 ) : BaseEntity()
