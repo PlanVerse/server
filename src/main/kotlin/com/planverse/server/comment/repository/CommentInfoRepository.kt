475550447a6231564d24a87e5c297fe4ffdfcafd
seoeungi <sjlim02255@gmail.com>
2025-04-12 03:32:48 +0900

    - 페이징처리된 리스트 조회 JPA 추가

--- src/main/kotlin/com/planverse/server/comment/repository/CommentInfoRepository.kt
+++ src/main/kotlin/com/planverse/server/comment/repository/CommentInfoRepository.kt
@@ -1,7 +1,10 @@
 package com.planverse.server.comment.repository
 
 import com.planverse.server.comment.entity.CommentInfoEntity
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface CommentInfoRepository : JpaRepository<CommentInfoEntity, Long> {
+    fun findAllByTargetAndTargetId(target: String, targetId: Long, pageable: Pageable): Slice<CommentInfoEntity>
 }
--- src/main/kotlin/com/planverse/server/reply/repository/ReplyInfoRepository.kt
+++ src/main/kotlin/com/planverse/server/reply/repository/ReplyInfoRepository.kt
@@ -1,7 +1,10 @@
 package com.planverse.server.reply.repository
 
 import com.planverse.server.reply.entity.ReplyInfoEntity
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ReplyInfoRepository : JpaRepository<ReplyInfoEntity, Long> {
+    fun findAllByCommentInfoId(commentId: Long, pageable: Pageable): Slice<ReplyInfoEntity>
 }
