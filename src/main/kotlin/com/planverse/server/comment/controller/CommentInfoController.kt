6829af0144e18ad2377f2d928c4442688ce48dc2
seoeungi <sjlim02255@gmail.com>
2025-04-16 13:39:03 +0900

    알림 관련 코드 임시 제거

--- src/main/kotlin/com/planverse/server/comment/controller/CommentInfoController.kt
+++ src/main/kotlin/com/planverse/server/comment/controller/CommentInfoController.kt
@@ -1,7 +1,6 @@
 package com.planverse.server.comment.controller
 
 import com.planverse.server.comment.service.CommentInfoService
-import com.planverse.server.common.controller.SubscribeController
 import com.planverse.server.common.dto.BaseResponse
 import com.planverse.server.user.dto.UserInfo
 import org.springframework.data.domain.Pageable
@@ -9,22 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
 
 @RestController
 @RequestMapping("/comment")
 class CommentInfoController(
     private val commentInfoService: CommentInfoService
-): SubscribeController {
+) {
 
     @GetMapping("/list/{targetId}")
     fun getCommentList(userInfo: UserInfo, @PathVariable(required = true) targetId: Long, pageable: Pageable): BaseResponse<Any> {
         val res = commentInfoService.getCommentList(userInfo, targetId, pageable)
 
         return BaseResponse.success(res)
-    }
-
-    override fun subscribe(userId: Long): SseEmitter {
-        return subscribe(userId)
     }
 }
--- src/main/kotlin/com/planverse/server/common/controller/SubscribeController.kt
+++
@@ -1,7 +0,0 @@
-package com.planverse.server.common.controller
-
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
-
-interface SubscribeController {
-    fun subscribe(userId: Long): SseEmitter
-}
